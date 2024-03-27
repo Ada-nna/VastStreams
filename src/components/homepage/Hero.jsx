71aa2a99054360e2a39b54ec74559a873e635f24
MUIZ OWOLABI <beasteye.official@gmail.com>
2024-03-27 06:53:08 +0100

    updated

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -27,22 +27,24 @@ const Hero = () => {
           <div className="">
             <HeroImage />
           </div>
-          <div className="flex flex-col justify-center place-items-center p-5">
+          <div className="flex flex-col justify-center place-items-center">
             <h1 className="font-bold text-[58px] text-center">
               The Best Streaming Experience
             </h1>
-            <p className="text-[18px] w-[62rem] text-center text-[#999999]">
-              VastStreams is the best streaming experience for watching your
-              favorite movies and shows on demand, anytime, anywhere. With
-              VastStreams, you can enjoy a wide variety of content, including
-              the latest blockbusters, classic movies, popular TV shows, and
-              more. You can also create your own watchlists, so you can easily
-              find the content you want to watch.
-            </p>
-            <div className="mt-5">
-              <Button />
-            </div>
           </div>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center place-items-center p-5">
+        <p className="text-[18px] w-[62rem] text-center text-[#999999]">
+          VastStreams is the best streaming experience for watching your
+          favorite movies and shows on demand, anytime, anywhere. With
+          VastStreams, you can enjoy a wide variety of content, including the
+          latest blockbusters, classic movies, popular TV shows, and more. You
+          can also create your own watchlists, so you can easily find the
+          content you want to watch.
+        </p>
+        <div className="mt-5">
+          <Button />
         </div>
       </div>
     </div>
